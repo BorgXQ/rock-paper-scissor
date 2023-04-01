a6997350099bec57094779f75ded657c2e02c5d1
BorgXQ <jonathan.widarsa@gmail.com>
2023-04-01 23:54:23 +0800

    Add console log for actual run

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -33,3 +33,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
